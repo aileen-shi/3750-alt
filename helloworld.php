6294f244c42f18594dc1f5961fce165d72c647fd
Aileen Shi <89747088+aileen-shi@users.noreply.github.com>
2024-06-01 17:57:35 -0400

    Add files via upload

---
+++ helloworld.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Prog 05</title>
+    </head>
+    <body>
+        <header>
+            <h1>Aileen Shi CPSC3750</h1>
+        </header>
+        <nav>
+            <iframe src="../navbar.html" style="width: 100%; height: 50px; border: none;"></iframe>
+        </nav>
+        <h1><?php echo "Hello World"; ?></h1>
+    </body>
+</html>

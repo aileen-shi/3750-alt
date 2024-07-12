a35932dcbac63d5a0a62c74dd5ec7bcc73e91a90
Aileen Shi <89747088+aileen-shi@users.noreply.github.com>
2024-07-12 16:00:03 -0400

    test quiz

--- prog12/ajax.js
+++ prog12/ajax.js
@@ -4,31 +4,23 @@ var ajaxreq = false,
   ajaxCallback;
 
 // ajaxRequest: Sets up a request
-function ajaxRequest(query) {
+function ajaxRequest(filename) {
   try {
     //make a new request object
     ajaxreq = new XMLHttpRequest();
   } catch (error) {
-    console.error("XMLHttpRequest failed to initialize", error);
     return false;
   }
-  var filename =
-    "https://aileenshi.com/prog12/search.php?query=" +
-    encodeURIComponent(query);
-  ajaxreq.open("GET", filename, true);
+  ajaxreq.open("GET", filename);
   ajaxreq.onreadystatechange = ajaxResponse;
-  try {
-    ajaxreq.send(null);
-  } catch (error) {
-    console.error("AJAX request failed to send", error);
-  }
+  ajaxreq.send(null);
 }
 
 // ajaxResponse: Waits for response and calls a function
 function ajaxResponse() {
   if (ajaxreq.readyState != 4) return;
   if (ajaxreq.status == 200) {
-    // if the request succeeded...
+    // if the request succeeded…
     if (ajaxCallback) ajaxCallback();
   } else alert("Request failed: " + ajaxreq.statusText);
   return true;

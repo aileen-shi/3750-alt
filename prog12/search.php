190d5456c55d65f91ca7e6b6c12bfcdaa6eff0a8
Aileen Shi <89747088+aileen-shi@users.noreply.github.com>
2024-07-12 16:18:29 -0400

    Update search.php

--- prog12/search.php
+++ prog12/search.php
@@ -8,19 +8,22 @@
    "Harvey Frank", "Inigo Montoya", "Jeff Austin",
    "Lynn Arlington", "Michael Washington", "Nancy West" );
 
+   $test = array("Alabam => Montgomery",
+   "Alaska => Juneau");
+
 echo "<?xml version=\"1.0\" ?>\n";
 echo "<names>\n";
 
 $query = isset($_GET['query']) ? $_GET['query'] : '';
 
 if (!empty($query)) {
-   // Loop through the names array
-   foreach ($names as $name) {
-      if (stristr($name, $query)) {
-         // Output matched names, using htmlspecialchars to prevent XML injection
-         echo "<name>" . htmlspecialchars($name, ENT_XML1, 'UTF-8') . "</name>\n";
-      }
-   }
+    // Loop through the names array
+    foreach ($names as $state => $capital) {
+        if (stristr($state, $query)) {
+            // Output matched state and capital, using htmlspecialchars to prevent XML injection
+            echo "<name>" . htmlspecialchars($state . ' - ' . $capital, ENT_XML1, 'UTF-8') . "</name>\n";
+        }
+    }
 }
 
 echo "</names>\n";

a1eb7b9c4a5391f2783fc5b6c0a0896aeb2330fa
Aileen Shi <89747088+aileen-shi@users.noreply.github.com>
2024-07-12 16:01:50 -0400

    original search

--- prog12/search.php
+++ prog12/search.php
@@ -1,74 +1,19 @@
 <?php
-   ini_set('display_errors, 1');
-   ini_set('display_startup_errors, 1');
-   error_reporting(E_ALL);
-
-   header("Content-type: text/xml");
-   $names = array (
-      "Alabama" => "Montgomery",
-    "Alaska" => "Juneau",
-    "Arizona" => "Phoenix",
-    "Arkansas" => "Little Rock",
-    "California" => "Sacramento",
-    "Colorado" => "Denver",
-    "Connecticut" => "Hartford",
-    "Delaware" => "Dover",
-    "Florida" => "Tallahassee",
-    "Georgia" => "Atlanta",
-    "Hawaii" => "Honolulu",
-    "Idaho" => "Boise",
-    "Illinois" => "Springfield",
-    "Indiana" => "Indianapolis",
-    "Iowa" => "Des Moines",
-    "Kansas" => "Topeka",
-    "Kentucky" => "Frankfort",
-    "Louisiana" => "Baton Rouge",
-    "Maine" => "Augusta",
-    "Maryland" => "Annapolis",
-    "Massachusetts" => "Boston",
-    "Michigan" => "Lansing",
-    "Minnesota" => "Saint Paul",
-    "Mississippi" => "Jackson",
-    "Missouri" => "Jefferson City",
-    "Montana" => "Helena",
-    "Nebraska" => "Lincoln",
-    "Nevada" => "Carson City",
-    "New Hampshire" => "Concord",
-    "New Jersey" => "Trenton",
-    "New Mexico" => "Santa Fe",
-    "New York" => "Albany",
-    "North Carolina" => "Raleigh",
-    "North Dakota" => "Bismarck",
-    "Ohio" => "Columbus",
-    "Oklahoma" => "Oklahoma City",
-    "Oregon" => "Salem",
-    "Pennsylvania" => "Harrisburg",
-    "Rhode Island" => "Providence",
-    "South Carolina" => "Columbia",
-    "South Dakota" => "Pierre",
-    "Tennesse" => "Nashville",
-    "Texas" => "Austin",
-    "Utah" => "Salt Lake City",
-    "Vermont" => "Montpelier",
-    "Virginia" => "Richmond",
-    "Washington" => "Olympia",
-    "West Virginia" => "Charleston",
-    "Wisconsin" => "Madison",
-    "Wyoming" => "Cheyenne"
-   );
+  header("Content-type: text/xml");
+  $names = array (
+   "John Smith", "John Jones", "Jane Smith", "Jane Tillman",
+   "Abraham Lincoln", "Sally Johnson", "Kilgore Trout",
+   "Bob Atkinson","Joe Cool", "Dorothy Barnes",
+   "Elizabeth Carlson", "Frank Dixon", "Gertrude East",
+   "Harvey Frank", "Inigo Montoya", "Jeff Austin",
+   "Lynn Arlington", "Michael Washington", "Nancy West" );
 
 echo "<?xml version=\"1.0\" ?>\n";
 echo "<names>\n";
-
-$query =  isset($_GET['query']) ? $_GET['query'] : '';
-
-if(!empty($query)) {
-   foreach ($names as $state => $capital) {
-      if (stristri($capital, $query)) {
-         echo "<name>" . htmlspecialchars($capital) . "</name>\n";
-      }
+while (list($k,$v)=each($names)) {
+   if (stristr($v,$_GET['query'])) {
+      echo "<name>$v</name>\n";
    }
 }
-
 echo "</names>\n";
 ?>

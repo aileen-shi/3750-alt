ae498330e5e7453df0c8488dac1c137eb84a8890
Aileen Shi <89747088+aileen-shi@users.noreply.github.com>
2024-07-12 16:21:54 -0400

    Update search.php

--- prog12/search.php
+++ prog12/search.php
@@ -1,15 +1,57 @@
 <?php
   header("Content-type: text/xml");
   $names = array(
-   "John Smith", "John Jones", "Jane Smith", "Jane Tillman",
-   "Abraham Lincoln", "Sally Johnson", "Kilgore Trout",
-   "Bob Atkinson","Joe Cool", "Dorothy Barnes",
-   "Elizabeth Carlson", "Frank Dixon", "Gertrude East",
-   "Harvey Frank", "Inigo Montoya", "Jeff Austin",
-   "Lynn Arlington", "Michael Washington", "Nancy West" );
-
-   $test = array("Alabam => Montgomery",
-   "Alaska => Juneau");
+   "Alabama" => "Montgomery",
+   "Alaska" => "Juneau",
+   "Arizona" => "Phoenix",
+   "Arkansas" => "Little Rock",
+   "California" => "Sacramento",
+   "Colorado" => "Denver",
+   "Connecticut" => "Hartford",
+   "Delaware" => "Dover",
+   "Florida" => "Tallahassee",
+   "Georgia" => "Atlanta",
+   "Hawaii" => "Honolulu",
+   "Idaho" => "Boise",
+   "Illinois" => "Springfield",
+   "Indiana" => "Indianapolis",
+   "Iowa" => "Des Moines",
+   "Kansas" => "Topeka",
+   "Kentucky" => "Frankfort",
+   "Louisiana" => "Baton Rouge",
+   "Maine" => "Augusta",
+   "Maryland" => "Annapolis",
+   "Massachusetts" => "Boston",
+   "Michigan" => "Lansing",
+   "Minnesota" => "Saint Paul",
+   "Mississippi" => "Jackson",
+   "Missouri" => "Jefferson City",
+   "Montana" => "Helena",
+   "Nebraska" => "Lincoln",
+   "Nevada" => "Carson City",
+   "New Hampshire" => "Concord",
+   "New Jersey" => "Trenton",
+   "New Mexico" => "Santa Fe",
+   "New York" => "Albany",
+   "North Carolina" => "Raleigh",
+   "North Dakota" => "Bismarck",
+   "Ohio" => "Columbus",
+   "Oklahoma" => "Oklahoma City",
+   "Oregon" => "Salem",
+   "Pennsylvania" => "Harrisburg",
+   "Rhode Island" => "Providence",
+   "South Carolina" => "Columbia",
+   "South Dakota" => "Pierre",
+   "Tennessee" => "Nashville",
+   "Texas" => "Austin",
+   "Utah" => "Salt Lake City",
+   "Vermont" => "Montpelier",
+   "Virginia" => "Richmond",
+   "Washington" => "Olympia",
+   "West Virginia" => "Charleston",
+   "Wisconsin" => "Madison",
+   "Wyoming" => "Cheyenne"
+  );
 
 echo "<?xml version=\"1.0\" ?>\n";
 echo "<names>\n";
@@ -18,10 +60,10 @@ $query = isset($_GET['query']) ? $_GET['query'] : '';
 
 if (!empty($query)) {
     // Loop through the names array
-    foreach ($test as $state => $capital) {
+    foreach ($names as $state => $capital) {
         if (stristr($state, $query)) {
             // Output matched state and capital, using htmlspecialchars to prevent XML injection
-            echo "<name>" . htmlspecialchars($state . ' - ' . $capital, ENT_XML1, 'UTF-8') . "</name>\n";
+            echo "<name>" . htmlspecialchars($capital, ENT_XML1, 'UTF-8') . "</name>\n";
         }
     }
 }

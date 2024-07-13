0c1e03ee856d2968179281b8679e704312d726a5
Aileen Shi <89747088+aileen-shi@users.noreply.github.com>
2024-07-13 01:31:41 -0400

    form

---
+++ group06/form.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Response</title>
+</head>
+<body>
+    <p><strong>?php echo $_POST['name']; ?</strong> has been registered</p>
+</body>
+</html>

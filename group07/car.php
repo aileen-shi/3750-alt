97665a96ca239c704e5188484640ab1fae9dcf8a
Aileen Shi <89747088+aileen-shi@users.noreply.github.com>
2024-07-13 14:47:13 -0400

    php sesssion

---
+++ group07/car.php
@@ -0,0 +1,45 @@
+<?php 
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Sessions</title>
+    <link rel="stylesheet" href="car.css" />
+  </head>
+  <body>
+    <h1>Car Sessions</h1>
+    <?php 
+    if (isset($_POST['cars'])) {
+      if (!empty($_SESSION['products'])) {
+          $products = array_unique(
+          array_merge(unserialize($_SESSION['products']),
+          $_POST['cars']));
+          $_SESSION['products'] = serialize($products);
+      } else {
+          $_SESSION['products'] = serialize($_POST['cars']);
+    }
+     echo "<p>Your cars have been registered!</p>";
+  }
+    ?>
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+      <label for="cars">Choose a car:</label>
+      <select name="cars" id="cars" size="7" multiple>
+        <option value="Clown">Clown</option>
+        <option value="Minion">Minion</option>
+        <option value="Fuzzy">Fuzzy</option>
+        <option value="Pea">Pea</option>
+        <option value="Pizza">Pizza</option>
+        <option value="Duck">Duck</option>
+        <option value="Fish">Fish</option>
+      </select>
+      <br /><br />
+      <input type="submit" value="Submit" />
+    </form>
+    <p>Select multiple with Ctrl or Command</p>
+    <p><a href="display.php">Go to Content Page</a></p>
+    <script src="../navbar.js"></script>
+  </body>
+</html>
---
+++ group07/display.php
@@ -0,0 +1,23 @@
+<?php 
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Your Cars</title>
+    </head>
+    <body>
+    <h1>Your Cars</h1>
+    <?php 
+    if (isset($_SESSION['products'])) {
+        echo "<strong>Your selection:</strong><ol>";
+        foreach(unserialize($_SESSION['products']) as $p) {
+            echo "<li>" . $p . "</li>";
+        }
+        echo "</ol>";
+    }
+    ?>
+    <p><a href="car.php">Return to Car Choice Page</a></p>
+    </body>
+</html>

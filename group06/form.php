f12c26d22263143611efecbc2fddea3f565a7005
Aileen Shi <89747088+aileen-shi@users.noreply.github.com>
2024-07-13 02:04:48 -0400

    Update form.php

--- group06/form.php
+++ group06/form.php
@@ -6,33 +6,9 @@
     <title>Form Response</title>
 </head>
 <body>
-    <h1>hello<h1>
-    <p><strong><?php echo $_POST['name']; ?></strong> has been registered</p>
-    <h2>Description:</h2>
-    <p><?php echo $_POST['description']; ?></p>
-    <h2>Password:</h2>
-    <?php echo $_POST['password']; ?>
-    <h2>Species:</h2>
-    <?php echo $_POST['species']; ?>
-    <h2>Conditions:</h2>
-    <?php 
-    if (!empty($_POST[health])) {
-        echo '<ul>';
-        foreach($_POST[health] as $condition) {
-            echo '<li>' . $condition . '</li>';
-        }
-        echo '</ul>';
-    }
-    else {
-        echo '<p>None</p>'
-    }
-    ?>
-    <h2>Heard About Us From:</h2>
-    <?php 
-    $ref = $_POST['ref'];
-    echo $_POST['ref'];
-    ?>
-    <h2>Pet Image</h2>
+    <p>
+    <?php echo "hello world" ?>
+    </p>
      <script src="../navbar.js"></script>
 </body>
 </html>

eb8ec634a2b6ac979611e6d061c932e17a803e28
Aileen Shi <89747088+aileen-shi@users.noreply.github.com>
2024-07-12 20:13:18 -0400

    added images and logic

--- group05/script.js
+++ group05/script.js
@@ -1,10 +1,16 @@
+// Current word
+let answer = "";
+let wrong = 0;
+let index = 0;
+let right = 0;
+
 function startGame() {
   // Fetch a new word from the server
   fetch("getWord.php")
     .then((response) => response.json())
     .then((data) => {
       if (data.word) {
-        //alert(data.word);
+        answer = data.word;
         setupGame(data.word);
       } else {
         console.error("Error fetching word:", data.error);
@@ -18,6 +24,11 @@ function setupGame(word) {
   if (document.getElementById("cheat").checked) {
     alert(word);
   }
+  // Initial image
+  const image = document.createElement("img");
+  image.id = "player";
+  image.src = "1.png";
+
   const wordToGuess = document.getElementById("wordToGuess");
   wordToGuess.innerHTML = "_ ".repeat(word.length).trim();
   generateLetterButtons();
@@ -37,9 +48,33 @@ function generateLetterButtons() {
 }
 
 function guessLetter(letter) {
-  console.log("Guessed letter:", letter);
-  // Implement the guessing logic here
-  // This is where you would update the displayed word or handle incorrect guesses
+  const image = document.getElementById("player");
+  const wordToGuess = document.getElementById("wordToGuess");
+  console.log(answer[index]);
+
+  // Incorrect guess
+  if (letter != answer[right]) {
+    wrong++;
+    // Update image
+    image.src = wrong + ".png";
+    console.log("incorrect ", letter);
+    // Game over
+    if (wrong == 11) {
+      alert("GAME OVER");
+    }
+  }
+  // Correct
+  else {
+    right++;
+    let remaining = answer;
+    for (let i = right; i < answer.length; i++) {
+      remaining[i] = " _";
+    }
+    wordToGuess.innerHTML = remaining;
+    if (right == answer.length) {
+      alert("YOU WIN");
+    }
+  }
 }
 
 // Initially start the game

9d592958dd27d4ced8aba8233e3dba7bff8aeae7
Aileen Shi <89747088+aileen-shi@users.noreply.github.com>
2024-07-13 19:13:46 -0400

    Update cookie.php

--- prog14/cookie.php
+++ prog14/cookie.php
@@ -96,15 +96,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 $none = False;
             }
             if (prime($num, 2)) {
-                if (file_put_contents('prime.txt', $num, "\n", FILE_APPEND !== false)) {
-                    echo "<p>Number written to armstrong.txt</p>";
-                } else {
-                    echo "<p>Failed to write number to armstrong.txt</p>";
+                $prime = fopen("prime.txt", "a");
+                if ($prime === false) {
+                    echo"<p>Failed to open</p>";
                 }
-                }
-                //$prime = fopen("prime.txt", "a");
-                //fwrite($prime, $num . "\n");
-                //fclose($prime);
+                fwrite($prime, $num . "\n");
+                fclose($prime);
                 $none = False;
             }
             if ($none) {

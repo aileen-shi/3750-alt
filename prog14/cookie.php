a2d247437451ea1c3211cc86e4fda410d01a0740
Aileen Shi <89747088+aileen-shi@users.noreply.github.com>
2024-07-13 16:56:58 -0400

    Update cookie.php

--- prog14/cookie.php
+++ prog14/cookie.php
@@ -15,19 +15,21 @@ if (!isset($_COOKIE['first'])) {
     
 }
 
+/*
 // Check form
 // Check these numbers
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     echo "<p>" . Entered FIRST IF ."</p>"
     if (isset($_POST['check'])) {
         echo "<p>" . Entered val in text area ."</p>"
-        /*
+        
         $nums = explode(' ', $_POST['check']);
     
         foreach($nums as $num) {
             echo "<p>" . $num ."</p>"
         }
-            */
+            
     }
 }
+*/
 ?>

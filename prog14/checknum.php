7448620a4db854b9b4c0c883a481873e1c667c2a
Aileen Shi <89747088+aileen-shi@users.noreply.github.com>
2024-07-13 22:19:35 -0400

    armstrong

--- prog14/checknum.php
+++ prog14/checknum.php
@@ -1,11 +1,38 @@
 <?php 
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $data = $_POST['text'];
+    if (isset($_POST['text'])) {
         $data = $_POST['text'];
-        if(empty($data)) {
-            echo "Empty";
-        }
-        else {
-            echo $data;
+        $nums = explode(" ", $data);
+
+        // Check each number
+        foreach($nums as $num) {
+            // Check if armstrong
+            if (armstrong($num)) {
+            // Write
+            $armstrong = fopen("armstrong.txt", "a");
+            fwrite($armstrong, $num . "\n");
+            fclose($armstrong);
+            echo "wrote to armstrong";
+            $none = False;
+            }
         }
     }
+}
+
+    // Check armstrong
+function armstrong($num) {
+    $sum = 0;
+    $n = strlen((string)$num);
+    $array = str_split($num);
+
+    for($i = 0; $i < $n; $i++) {
+        $temp = pow((int)$array[$i], $n);
+        //echo "<p>" . $temp . "</p>";
+        // Update Sum
+        $sum += $temp;
+    }
+
+    return $sum == $num;
+}
 ?>

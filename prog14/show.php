573828e50a72fa5072f96c98bc8b9ee2f97bb0b3
Aileen Shi <89747088+aileen-shi@users.noreply.github.com>
2024-07-13 22:46:02 -0400

    Update show.php

--- prog14/show.php
+++ prog14/show.php
@@ -3,6 +3,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_POST['type'])) {
         $type = $_POST['type'];
         $file = $type . "txt";
+        echo $file . "\n";
 
         if (file_exists($file)) {
             $fileData = fopen($file, "r");

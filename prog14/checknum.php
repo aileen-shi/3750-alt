de941d3b13cc2ce3a82d65d1d04470cf4eafb8b0
Aileen Shi <89747088+aileen-shi@users.noreply.github.com>
2024-07-13 22:21:55 -0400

    Update checknum.php

--- prog14/checknum.php
+++ prog14/checknum.php
@@ -7,20 +7,30 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
         // Check each number
         foreach($nums as $num) {
+            $none = True;
             // Check if armstrong
             if (armstrong($num)) {
-            // Write
-            $armstrong = fopen("armstrong.txt", "a");
-            fwrite($armstrong, $num . "\n");
-            fclose($armstrong);
-            echo "wrote to armstrong";
-            $none = False;
+                // Write
+                $armstrong = fopen("armstrong.txt", "a");
+                fwrite($armstrong, $num . "\n");
+                fclose($armstrong);
+                echo "wrote to armstrong";
+                $none = False;
+            }
+            // Check fibonacci
+            if (fibonacci($num)) {
+                // Write
+                $fibonacci = fopen("fibonacci.txt", "a");
+                fwrite($fibonacci, $num . "\n");
+                fclose($fibonacci);
+                echo "wrote to fib";
+                $none = False;
             }
         }
     }
 }
 
-    // Check armstrong
+// Check armstrong
 function armstrong($num) {
     $sum = 0;
     $n = strlen((string)$num);
@@ -34,5 +44,17 @@ function armstrong($num) {
     }
 
     return $sum == $num;
+}
+
+// Check fibonacci
+function fibonacci($num) {
+    $a = 0;
+    $b = 1;
+    while($a < $num) {
+        $temp = $a;
+        $a = $b;
+        $b += $temp;
+    }
+    return $a == $num;
 }
 ?>

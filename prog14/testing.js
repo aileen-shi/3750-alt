be54a834037a79f74fd18a3b13ca343c628eb0e0
Aileen Shi <89747088+aileen-shi@users.noreply.github.com>
2024-07-13 21:54:27 -0400

    Update testing.js

--- prog14/testing.js
+++ prog14/testing.js
@@ -1,14 +1,13 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   function initializeCookie() {
     var request = new XMLHttpRequest();
-    request.onreadystatechange ==
-      function () {
-        if (this.readyState == 4 && this.status == 200) {
-          console.log("requested php", request.responseText);
-        } else {
-          console.log("error", request.statusText);
-        }
-      };
+    request.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        console.log("requested php", request.responseText);
+      } else {
+        console.log("error", request.statusText);
+      }
+    };
     request.open("GET", "test.php", true);
     request.send();
   }

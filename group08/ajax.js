cdebe02f34cdaca1b90630c61b21a88cee8937b9
Aileen Shi <89747088+aileen-shi@users.noreply.github.com>
2024-07-19 18:29:03 -0400

    Update ajax.js

--- group08/ajax.js
+++ group08/ajax.js
@@ -7,37 +7,47 @@
 
 console.log("loaded");
 
-document.getElementById("zipForm").addEventListener("submit", function (event) {
-  event.preventDefault();
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("zipForm")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
 
-  console.log("HERE");
-  // Get zipcodes to send
-  zip1 = document.getElementById("zip1").value;
-  zip2 = document.getElementById("zip2").value;
+      console.log("HERE");
+      // Get zipcodes to send
+      zip1 = document.getElementById("zip1").value;
+      zip2 = document.getElementById("zip2").value;
 
-  var request = new XMLHttpRequest();
-  /*
-  // Callback
-  request.onload = function () {
-    if (request.status >= 200 && request.status < 400) {
-      console.log("success");
-      console.log(request.responseText);
-    } else {
-      console.error("failed with status:", request.status);
-    }
-  };
-  */
-  // Send
-  request.open("POST", "zipcode.php", true);
-  request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+      var request = new XMLHttpRequest();
+      /*
+        // Callback
+        request.onload = function () {
+            if (request.status >= 200 && request.status < 400) {
+            console.log("success");
+            console.log(request.responseText);
+            } else {
+            console.error("failed with status:", request.status);
+            }
+        };
+        */
+      // Send
+      request.open("POST", "zipcode.php", true);
+      request.setRequestHeader(
+        "Content-Type",
+        "application/x-www-form-urlencoded"
+      );
 
-  request.onreadystatechange = function () {
-    if (request.status === 4 && request.status === 200) {
-      document.getElementById("result").innerText = request.responseText;
-    }
-  };
+      request.onreadystatechange = function () {
+        if (request.status === 4 && request.status === 200) {
+          document.getElementById("result").innerText = request.responseText;
+        }
+      };
 
-  var params =
-    "zip1=" + encodeURIComponent(zip1) + "&zip2=" + encodeURIComponent(zip2);
-  request.send(params);
+      var params =
+        "zip1=" +
+        encodeURIComponent(zip1) +
+        "&zip2=" +
+        encodeURIComponent(zip2);
+      request.send(params);
+    });
 });

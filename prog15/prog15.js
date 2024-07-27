9621e9acd1701ffcc6474813863f103fd58bd1fb
Aileen Shi <89747088+aileen-shi@users.noreply.github.com>
2024-07-27 13:54:24 -0400

    update

---
+++ prog15/prog15.js
@@ -0,0 +1,27 @@
+/*
+    Aileen Shi
+    CPSC 3750
+    7/27/24
+    Integrate Database
+*/
+
+//
+document.getElementById("record-form").addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    data = new FormData(document.getElementById("record-form"));
+
+    request = new XMLHttpRequest();
+
+    request.onreadystatechange = function() {
+        if (request.readyState === 4 && request.status === 200) {
+            console.log(request.responseText);
+        }
+        else {
+            console.log("error", request.statusText);
+        }
+    };
+    
+    request.open("POST", "insert.php", true);
+    request.send(FormData);
+});

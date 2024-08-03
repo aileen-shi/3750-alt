aa10a630cd7e9ccf5bdf8250b6938f8a981b139d
Aileen Shi <89747088+aileen-shi@users.noreply.github.com>
2024-08-02 22:28:41 -0400

    Update login.php

--- project2/login.php
+++ project2/login.php
@@ -1,5 +1,7 @@
 <?php 
     //session_start();
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
 
     // Connect
     $mysqli = mysqli_connect("192.81.214.86", "aileen", "i-heart-deftones-and-cats", "cpsc_3750");
@@ -16,17 +18,22 @@
         $clean_pass = mysqli_real_escape_string($mysqli, $_POST['password']);
 
         // Query
-        $sql = "SELECT * FROM User_Login WHERE username=$clean_user AND pass=$clean_pass";
+        $sql = "SELECT * FROM User_Login WHERE username='$clean_user' AND pass='$clean_pass'";
         $res = mysqli_query($mysqli, $sql);
 
         // Handle data
-        if (mysqli_num_rows($res) > 0) {
-            echo "Success";
-        } else {
-            echo "Failed";    
+        if ($res) {
+            if (mysqli_num_rows($res) > 0) {
+                echo "Success";
+            } else {
+                echo "Failed";    
+            }
+        }
+        else {
+            echo "Query error";
         }
 
-        mysqli_free_result($res);
+
         mysqli_close($mysqli);
     }
 ?>

0b2ff803d20b999fb10f733d10dad6ea9a686a79
Aileen Shi <89747088+aileen-shi@users.noreply.github.com>
2024-08-02 21:59:50 -0400

    test login

--- project2/login.js
+++ project2/login.js
@@ -1,8 +1,38 @@
 document.addEventListener("DOMContentLoaded", function () {
     const register = document.getElementById("register");
+    const login = document.getElementById("");
 
     register.addEventListener("click", function(event) {
-        window.location.assign("https://aileenshi.com/project2/register.html");
+        //window.location.assign("https://aileenshi.com/project2/register.html");
+        window.location.href = "register.html";
+    });
+
+    // Send register form to php
+    document.getElementById("login-form").addEventListener("submit", function(event) {
+        event.preventDefault();
+        data = new FormData(document.getElementById("login-form"));
+
+        request = new XMLHttpRequest();
+
+        // Response
+        request.onreadystatechange = function() {
+            if (request.readyState === 4 && request.status === 200) {
+                console.log(request.responseText);
+                if (request.responseText == "Success") {
+                    window.location.href = "account.html";
+                }
+                else {
+                    alert("Incorrect user and/or password");
+                }
+            }
+            else {
+                console.log("error", request.statusText);
+            }
+        };
+
+        // Send request
+        request.open("POST", "login.php", true);
+        request.send(data);
     });
 });
 
--- project2/login.php
+++ project2/login.php
@@ -1,3 +1,32 @@
 <?php 
     session_start();
+
+    // Connect
+    $mysqli = mysqli_connect("192.81.214.86", "aileen", "i-heart-deftones-and-cats", "cpsc_3750");
+
+    // Error
+    if (mysqli_connect_errno()) {
+        printf("Connect failed: %s\n", mysqli_connect_error());
+        exit();
+    } 
+    // Success
+    else {
+        // Clean
+        $clean_user = mysqli_real_escape_string($mysqli, $_POST['username']);
+        $clean_pass = mysqli_real_escape_string($mysqli, $_POST['password']);
+
+        // Query
+        $sql = "SELECT * FROM User_Login WHERE username=$clean_user AND pass=$clean_pass";
+        $res = mysqli_query($mysqli, $sql);
+
+        // Handle data
+        if (mysqli_num_rows($res) > 0) {
+            echo "Success";
+        } else {
+            echo "Failed";    
+        }
+
+        mysqli_free_result($res);
+        mysqli_close($mysqli);
+    }
 ?>

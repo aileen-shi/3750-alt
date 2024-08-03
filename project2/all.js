a816e04dadcf94e2f85874a5221a8494b086196b
Aileen Shi <89747088+aileen-shi@users.noreply.github.com>
2024-08-03 18:03:55 -0400

    debug

--- project2/all.js
+++ project2/all.js
@@ -7,8 +7,9 @@ function viewAll() {
     request = new XMLHttpRequest();
     request.onreadystatechange = function() {
         if (request.readyState === 4 && request.status === 200) {
+            console.log(request.responseText);
             entries = JSON.parse(request.responseText);
-            show(entries);
+            //show(entries);
         }
         else {
             console.log("error", request.statusText);
